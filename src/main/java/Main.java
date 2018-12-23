8b54faac676c2ef515d5942abb5acbc1a659536b
Angelika Serwa <graszka223@gmail.com>
2018-12-23 01:20:08 +0100

    Solve cors issue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,10 @@ import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(7000);
+        Javalin app = Javalin.create();
+        app.enableCorsForOrigin("http://localhost:300");
+        app.start(7000);
+
         app.post("/register", new RegisterRequestHandler());
         app.post("/login", new LoginRequestHandler());
         Javalin.create().ws("/chat", new WebSocketHandler()).start(7070);
